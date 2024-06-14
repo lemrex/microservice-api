8794f4f209ab8e4f4fd785d503c51bf4e31add3d
lemrex <emekaralf@gmail.com>
2024-06-14 18:05:05 +0100

    edit app.js for frontend

--- frontend/app.js
+++ frontend/app.js
@@ -6,15 +6,15 @@ const port = 3000;
 app.use(express.json());
 
 app.post('/', async (req, res) => {
-  const { username } = req.body;
+//  const { username } = req.body;
   try {
     const [dateRes, timeRes, countryRes] = await Promise.all([
-      axios.get('http://date/date'),
-      axios.get('http://time/time'),
-      axios.get('http://country/country')
+      axios.get('http://date.lemrex.online'),
+      axios.get('http://time.lemrex.online'),
+      axios.get('http://country.lemrex.online')
     ]);
 
-    const greeting = `Hello ${username}, today's date is ${dateRes.data.date} and the time is ${timeRes.data.time} in ${countryRes.data.country}.`;
+    const greeting = `Hello, today's date is ${dateRes.data.date} and the time is ${timeRes.data.time} in ${countryRes.data.country}.`;
     res.send({ message: greeting });
   } catch (error) {
     res.status(500).send({ error: 'Failed to retrieve data' });
